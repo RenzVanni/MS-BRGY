805c8f327c39fee8bc57d7c7da87410ccde62fda
Renz <renzvanni.dev@gmail.com>
2025-09-07 03:47:31 +0800

    fix gateway for render

--- gateway/src/main/java/com/ms_spring_brgy/gateway/config/Security_Config.java
+++ gateway/src/main/java/com/ms_spring_brgy/gateway/config/Security_Config.java
@@ -13,16 +13,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Mono;
 
@@ -47,11 +41,16 @@ public class Security_Config {
                 .oauth2ResourceServer(customizer -> customizer
                         .bearerTokenConverter(new CookieConverter())
                         .jwt(jwtConfigurer -> jwtConfigurer
-                                .jwtAuthenticationConverter(new ReactiveJwtAuthenticationConverterAdapter(jwtAuthConverter)))
+                                .jwtAuthenticationConverter(reactiveJwtAuthConverter(jwtAuthConverter)))
                         .authenticationEntryPoint(cLearExpiredCookie));
 
 
         return http.build();
+    }
+
+    @Bean
+    public Converter<Jwt, ? extends Mono<? extends AbstractAuthenticationToken>> reactiveJwtAuthConverter(JwtAuthConverter jwtAuthConverter) {
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthConverter);
     }
 
     @Bean
